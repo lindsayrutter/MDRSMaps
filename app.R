924a09349c4c1c720d2a36626890678f1e71451a
lrutter <lindsayannerutter@gmail.com>
2018-12-14 02:04:43 -0700

    Fix about tab

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ library(shinycssloaders)
 
 
 
-options(spinner.color.background="#F5F5F5")
+options(spinner.color.background="#FF0000")
 
 
 
@@ -22,8 +22,9 @@ shinydashboard::menuItem("About", tabName = "about", selected=TRUE)
 
 
 
+
 body <- shinydashboard::dashboardBody(
-shinydashboard::tabItems(
+    shinydashboard::tabItems(
 shinydashboard::tabItem(tabName = "mapPlot",
                         
                         
@@ -42,11 +43,9 @@ border-top-color:#D3D3D3;
 background:#D3D3D3
 }
 
-")),                        
-                        
-                        
-                        
-                        
+")),
+                    
+                  
 fluidRow(
 column(width = 4, 
 
@@ -58,7 +57,19 @@ fileInput('datafile', 'Choose CSV File',
 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')))),
 
 column(width = 8,
-shinydashboard::box(width = NULL, shinycssloaders::withSpinner(plotOutput("plotMap")), collapsible = FALSE, background = "black", title = "MDRS map with overlaid coordinates", status = "primary", solidHeader = TRUE))))))
+shinydashboard::box(width = NULL, shinycssloaders::withSpinner(plotOutput("plotMap"), color = "#990000"), collapsible = FALSE, background = "black", title = "MDRS map with overlaid coordinates", status = "primary", solidHeader = TRUE)))),
+
+shinydashboard::tabItem(tabName = "about",
+shiny::fluidRow("Run by the Mars Society, the Mars Desert Research Station is a space analogue habitat in the deserts of Utah. Each year, crews of about seven members spend two weeks at the facility simulating a Mars mission. Part of this simulation invovles extravehicular activities (EVAs), where crew members done spacesuits and investigate the surroundings of the habitat.", style='padding:10px;'),
+shiny::fluidRow("This application is intended for crew members to overlay information obtained during their EVAs onto maps. To use this application, you simply need to have GPS coordinates saved from your EVAs. You can then simply select a .CSV file from your local computer that contains latitude and longitude measurements. The application will then plot these latitude and longitude measurements onto a map surrounding the habitat.", style='padding:10px;'),
+
+shiny::fluidRow("Specifically, the CSV file must contain a minimum of three .", style='padding:10px;'))))
+
+
+
+
+
+
 
 
 ui <- shinydashboard::dashboardPage(skin = "red",
@@ -68,7 +79,7 @@ body
 )
 
 
-server=function(input, output){
+server=function(input, output, session){
 
 #This function is repsonsible for loading in the selected file
 filedata <- reactive({
@@ -103,4 +114,4 @@ return(p)
 
 })}
 
-shinyApp(ui, server)
+shiny::shinyApp(ui = ui, server = server)
