289cda35c71404024dd735c5fe7cbcad04429923
lrutter <lindsayannerutter@gmail.com>
2018-12-14 01:23:03 -0700

    Minimum working app

--- app.R
+++ app.R
@@ -8,13 +8,65 @@ library(shinycssloaders)
 options(spinner.color.background="#F5F5F5")
 
 
-ui=pageWithSidebar(
-headerPanel('Simple matrixInput'),
-sidebarPanel(
-fileInput('datafile', 'Choose CSV File',
-accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))),
 
-mainPanel(plotOutput(outputId = 'plotMap')))
+
+sidebar <- shinydashboard::dashboardSidebar(
+width = 180,
+hr(),
+shinydashboard::sidebarMenu(id="tabs",
+shinydashboard::menuItem("Application", tabName="mapPlot"),
+shinydashboard::menuItem("About", tabName = "about", selected=TRUE)
+)
+)
+
+
+
+
+body <- shinydashboard::dashboardBody(
+shinydashboard::tabItems(
+shinydashboard::tabItem(tabName = "mapPlot",
+                        
+                        
+tags$style(HTML("
+
+.box.box-solid.box-primary>.box-header {
+color:#fff;
+background:#222d32
+}
+
+.box.box-solid.box-primary{
+border-bottom-color:#D3D3D3;
+border-left-color:#D3D3D3;
+border-right-color:#D3D3D3;
+border-top-color:#D3D3D3;
+background:#D3D3D3
+}
+
+")),                        
+                        
+                        
+                        
+                        
+fluidRow(
+column(width = 4, 
+
+shinydashboard::box(width = NULL, status = "primary", title = "Plot metrics", solidHeader = TRUE,
+# shiny::selectizeInput("selPair", "Treatment pairs:", choices = myPairs, multiple = TRUE, options = list(maxItems = 2)),
+# shiny::selectInput("selOrder", "Metric order:", choices = c("Increasing", "Decreasing")),
+# shiny::numericInput("binSize", "Hexagon size:", value = 10, min = 1),
+fileInput('datafile', 'Choose CSV File',
+accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')))),
+
+column(width = 8,
+shinydashboard::box(width = NULL, shinycssloaders::withSpinner(plotOutput("plotMap")), collapsible = FALSE, background = "black", title = "MDRS map with overlaid coordinates", status = "primary", solidHeader = TRUE))))))
+
+
+ui <- shinydashboard::dashboardPage(skin = "red",
+shinydashboard::dashboardHeader(title = "MDRS Map", titleWidth = 180),
+sidebar,
+body
+)
+
 
 server=function(input, output){
 
