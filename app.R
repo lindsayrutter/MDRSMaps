a28667730055af87653eb5616bc05a9162f39575
lrutter <lindsayannerutter@gmail.com>
2018-12-17 10:41:28 -0700

    Fixed geom_line

--- app.R
+++ app.R
@@ -4,35 +4,29 @@ library(shinydashboard)
 library(shinycssloaders)
 library(ggmap)
 library(ggplot2)
-library(plotly)
+#library(plotly)
 
 options(spinner.color.background="#FF0000")
 
 sidebar <- shinydashboard::dashboardSidebar(
-width = 180,
-hr(),
-shinydashboard::sidebarMenu(id="tabs",
-shinydashboard::menuItem("Application", tabName="mapPlot"),
-shinydashboard::menuItem("Instructions", tabName = "about", selected=TRUE)
+    width = 180,
+    hr(),
+    shinydashboard::sidebarMenu(id="tabs",
+    shinydashboard::menuItem("Application", tabName="mapPlot"),
+    shinydashboard::menuItem("Instructions", tabName = "about", selected=TRUE)
+    )
 )
-)
-
-
-
-
 
 body <- shinydashboard::dashboardBody(
-    shinydashboard::tabItems(
+shinydashboard::tabItems(
 shinydashboard::tabItem(tabName = "mapPlot",
-                        
-                        
-tags$style(HTML("
 
+
+tags$style(HTML("
 .box.box-solid.box-primary>.box-header {
 color:#fff;
 background:#222d32
 }
-
 .box.box-solid.box-primary{
 border-bottom-color:#D3D3D3;
 border-left-color:#D3D3D3;
@@ -40,27 +34,21 @@ border-right-color:#D3D3D3;
 border-top-color:#D3D3D3;
 background:#D3D3D3
 }
-
 ")),
-                    
-                  
+
+
 fluidRow(
 column(width = 4, 
+
 shinydashboard::box(width = NULL, status = "primary", title = "Plot metrics", solidHeader = TRUE,
+# shiny::selectizeInput("selPair", "Treatment pairs:", choices = myPairs, multiple = TRUE, options = list(maxItems = 2)),
+# shiny::selectInput("selOrder", "Metric order:", choices = c("Increasing", "Decreasing")),
+# shiny::numericInput("binSize", "Hexagon size:", value = 10, min = 1),
 fileInput('datafile', 'Choose CSV File',
 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv')))),
 
-column(width = 4,
-shinydashboard::box(width = NULL, status = "primary", title = "Latitude", solidHeader = TRUE,
-uiOutput("outMinLat"), uiOutput("outMedLat"), uiOutput("outMaxLat"))),
-
-column(width = 4,
-shinydashboard::box(width = NULL, status = "primary", title = "Latitude", solidHeader = TRUE,
-uiOutput("outMinLon"), uiOutput("outMedLon"), uiOutput("outMaxLon")))),
-
-fluidRow(
-column(width = 12,
-shinydashboard::box(width = NULL, shinycssloaders::withSpinner(plotlyOutput("plotlyMap"), color = "#990000"), collapsible = FALSE, background = "black", title = "MDRS map with overlaid coordinates", status = "primary", solidHeader = TRUE)))),
+column(width = 8,
+shinydashboard::box(width = NULL, shinycssloaders::withSpinner(plotOutput("plotMap"), color = "#990000"), collapsible = FALSE, background = "black", title = "MDRS map with overlaid coordinates", status = "primary", solidHeader = TRUE)))),
 
 shinydashboard::tabItem(tabName = "about",
 shiny::fluidRow("Run by the Mars Society, the Mars Desert Research Station is a space analogue habitat in the deserts of Utah. Each year, crews of about seven members spend two weeks at the facility simulating a Mars mission. Part of this simulation invovles extravehicular activities (EVAs), where crew members done spacesuits and investigate the surroundings of the habitat.", style='padding:10px;'),
@@ -94,67 +82,34 @@ body
 
 server=function(input, output, session){
 
-filedata <- eventReactive(input$datafile, {
-    infile <- input$datafile
-    validate(need(!is.null(infile), "Select a file."))
-    read.csv(infile$datapath)
-}, ignoreNULL = FALSE)
-
-df <- eventReactive(filedata(), {
-    validate(need(!is.null(df), "Select a file."))
-    df <-filedata()
-    df <- as.data.frame(df)
-    df <- df[,1:ncol(df)]
-    df <- setNames(df, c("ID", "Latitude", "Longitude"))
-    df
-}, ignoreNULL = FALSE)
-
-
-output$outMinLat <- renderUI({
-    minLat = min(df()$Latitude)
-    numericInput("minLat", "Update Minimum", value = minLat)
-})
-
-output$outMedLat <- renderUI({
-    medLat = mean(df()$Latitude)
-    numericInput("medLat", "Update Center", value = medLat)
-})
-
-output$outMaxLat <- renderUI({
-    maxLat = max(df()$Latitude)
-    numericInput("maxLat", "Update Maximum", value = maxLat)
-})
-
-output$outMinLon <- renderUI({
-    minLon = min(df()$Longitude)
-    numericInput("minLon", "Update Minimum", value = minLon)
-})
-
-output$outMedLon <- renderUI({
-    medLon = mean(df()$Longitude)
-    numericInput("medLon", "Update Center", value = medLon)
-})
-
-output$outMaxLon <- renderUI({
-    maxLon = max(df()$Longitude)
-    numericInput("maxLon", "Update Maximum", value = maxLon)
+#This function is repsonsible for loading in the selected file
+filedata <- reactive({
+infile <- input$datafile
+if (is.null(infile)) {
+# User has not uploaded a file yet
+return(NULL)
+}
+read.csv(infile$datapath)
 })
 
 
+output$plotMap <- renderPlot({
 
-output$plotlyMap <- renderPlotly({
+df <-filedata()
+if (is.null(df)) return(NULL)
+
+df <- as.data.frame(df)
+df <- df[,1:ncol(df)]
+df <- setNames(df, c("ID", "Latitude", "Longitude"))
 
 register_google(key = "AIzaSyCcJu4DttxEDccaixZomOCXcUhptYHX2n4")
 # Center on MDRS
-island = get_map(location = c(lon = -110.7919, lat = 38.4065), zoom = 13, maptype = "satellite")
+bbox <- ggmap::make_bbox(lon=Longitude, lat=Latitude, data, f = 0.05)
+mapLoc <- get_map(location = bbox, zoom = 13, maptype = "satellite")
 
-p <- ggmap(island, extent = "panel", legend = "bottomright") +
-    geom_point(aes(x = Longitude, y = Latitude), data = df(), size = 4, color = "#ff0000") + xlab("Longitude") + ylab("Latitude")
-#+ scale_x_continuous(limits = c(minLon, maxLon), expand = c(0, 0)) +
-#scale_y_continuous(limits = c(minLat, maxLat), expand = c(0, 0))
-gp <- ggplotly(p)
+p <- ggmap(mapLoc, extent = "panel", legend = "bottomright") + geom_line(aes(x = lon, y = lat), data=data) + geom_point(aes(x = lon, y = lat, size = size, color = color), data = data) + scale_color_identity() + theme(legend.position="none") + xlab("Longitude") + ylab("Latitude") + scale_size_identity()
 
-return(gp)
+return(p)
 
 })}
 
